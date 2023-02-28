8740d1e3e3a65da5484d7b5ab035b7f4cba68758
Richard Truong <reket1990@yahoo.com>
2023-02-28 05:11:42 -0800

    update skills to be more versatile

--- src/modules/Skills.jsx
+++ src/modules/Skills.jsx
@@ -4,18 +4,289 @@ import './Skills.css';
 import Quote from './submodules/Quote';
 import SkillPair from './submodules/SkillPair';
 
-const languages = [{
-  name: 'English',
-},
-{
-  name: 'Chinese - Teo Chew',
-},
-{
-  name: 'Vietnamese',
-},
-{
-  name: 'Chinese - Mandarin',
-}];
+const breadth = [
+  {
+    name: '7000+ Hours',
+    games: [{
+      name: 'StarCraft 2',
+    }, {
+      name: 'starcraft2.com/en-us/profile/1/1/2735594',
+    }],
+  },
+  {
+    name: '3000+ Hours',
+    games: [{
+      name: 'League of Legends',
+    }, {
+      name: 'www.op.gg/summoners/na/qxc',
+    }],
+  },
+  {
+    name: '1200+ Hours',
+    games: [{
+      name: 'Destiny 2',
+    }, {
+      name: 'raid.report/pc/4611686018468430152',
+    }],
+  },
+  {
+    name: '800+ Hours',
+    games: [{
+      name: 'Path of Exile',
+    }, {
+      name: 'Super Smash Bros. Melee',
+    }],
+  },
+  {
+    name: '300+ Hours',
+    games: [{
+      name: 'New World',
+    },
+    {
+      name: 'Xcom 2',
+    },
+    {
+      name: 'Hearthstone',
+    },
+    {
+      name: 'Heroes of the Storm',
+    },
+    {
+      name: 'Diablo 2',
+    },
+    {
+      name: 'Counter-Strike Source',
+    }],
+  },
+  {
+    name: '150+ Hours',
+    games: [{
+      name: 'Diablo 3',
+    },
+    {
+      name: 'Slay the Spire',
+    },
+    {
+      name: 'Xcom: Enemy Unknown',
+    },
+    {
+      name: 'The Cycle: Frontier',
+    },
+    {
+      name: 'Overwatch',
+    },
+    {
+      name: 'Guilty Gear Strive',
+    },
+    {
+      name: 'Warcraft 3',
+    },
+    {
+      name: 'Guitar Hero 3',
+    }],
+  },
+  {
+    name: '50+ Hours',
+    games: [{
+      name: 'Lost Ark',
+    },
+    {
+      name: 'Dota Underlords',
+    },
+    {
+      name: 'Teamfight Tactics',
+    },
+    {
+      name: 'Civilization V',
+    },
+    {
+      name: 'Europa Universalis IV',
+    },
+    {
+      name: 'Age of Empires IV',
+    },
+    {
+      name: 'Witcher 3',
+    },
+    {
+      name: 'Company of Heroes',
+    },
+    {
+      name: 'Offworld Trading Company',
+    },
+    {
+      name: 'Monster Train',
+    },
+    {
+      name: 'Factorio',
+    },
+    {
+      name: 'Marvel Snap',
+    },
+    {
+      name: 'Team Fortress 2',
+    },
+    {
+      name: 'Super Smash Bros. Ultimate',
+    },
+    {
+      name: 'Beat Saber',
+    }],
+  },
+  {
+    name: '20+ Hours',
+    games: [{
+      name: 'Left 4 dead 2',
+    },
+    {
+      name: 'Battle Brothers',
+    },
+    {
+      name: 'Counter-Strike GO',
+    },
+    {
+      name: 'Valorant',
+    },
+    {
+      name: 'Dungeon Defenders',
+    },
+    {
+      name: 'Stellaris',
+    },
+    {
+      name: 'Monster Hunter World',
+    },
+    {
+      name: 'Prismata',
+    },
+    {
+      name: 'They are Billions',
+    },
+    {
+      name: 'Stardew Valley',
+    },
+    {
+      name: 'Civilization 6',
+    },
+    {
+      name: 'Gunfire Reborn',
+    },
+    {
+      name: 'Wargroove',
+    },
+    {
+      name: 'Back 4 Blood',
+    },
+    {
+      name: 'HunieCam Studio',
+    },
+    {
+      name: 'Vampire Survivors',
+    },
+    {
+      name: 'Othercide',
+    },
+    {
+      name: 'Darkest Dungeon',
+    },
+    {
+      name: 'Battlerite',
+    },
+    {
+      name: 'Dark Souls',
+    },
+    {
+      name: 'Orcs Must Die 2',
+    },
+    {
+      name: 'Defender\'s Quest',
+    },
+    {
+      name: 'Supreme Commander 2',
+    },
+    {
+      name: 'Street Fighter IV',
+    },
+    {
+      name: 'Monaco',
+    },
+    {
+      name: 'Starbound',
+    },
+    {
+      name: 'Warhammer 40K: Darktide',
+    }],
+  },
+];
+
+const mastery = [
+  {
+    name: 'Player vs Player',
+    games: [{
+      name: 'Overwatch Masters Rank',
+    },
+    {
+      name: 'Starcraft 2 top ~200 in world',
+    },
+    {
+      name: 'League of Legends Master rank',
+    },
+    {
+      name: 'Heroes of the Storm Top 60 in North America',
+    },
+    {
+      name: 'Hearthstone Rank 5',
+    },
+    {
+      name: 'Duelyst Legend Rank',
+    },
+    {
+      name: 'Prismata Top 15 in world',
+    },
+    {
+      name: 'Guilty Gear Strive Celestial Rank',
+    },
+    {
+      name: 'Team Fight Tactics Platinum IV Rank',
+    },
+    {
+      name: 'Starcraft: Brood War B- Rank on Iccup',
+    },
+    {
+      name: 'Age of empires IV top 700',
+    },
+    {
+      name: 'Marvel Snap Rank 85',
+    },
+    {
+      name: 'Clash Royale - Won IEM weekly tournament',
+    }],
+  },
+  {
+    name: 'Player vs Game',
+    games: [{
+      name: 'Destiny 2 Completed 3 raids in contest mode, 2 raids with fireteam of 3, 2 raids with fireteam of 2, and 5 dungeons solo',
+    },
+    {
+      name: 'Xcom: Enemy Unknown completed campaign on classic difficult & ironman',
+    },
+    {
+      name: 'Xcom 2: Completed campaign on highest difficulty & ironman',
+    },
+    {
+      name: 'Diablo 3 Reached Greater Rift 70 in Hardcore',
+    },
+    {
+      name: 'Path of Exile Reached Maps in Solo Self Found Hardcore, Zizaran Gauntlet',
+    },
+    {
+      name: 'Slay the Spire Ascension 14',
+    },
+    {
+      name: 'Guitar Hero 3 Completed Through the Fire and Flames on Expert',
+    }],
+  },
+];
 
 // Group skills in pairs
 const groupPairs = (input) => {
@@ -32,8 +303,6 @@ const groupPairs = (input) => {
   return output;
 };
 
-const languagePairs = groupPairs(languages);
-
 function Skills() {
   return (
     <div>
@@ -44,8 +313,15 @@ function Skills() {
             quote="I play games."
           />
           <hr />
-          <h3>Languages</h3>
-          {languagePairs.map(([language1, language2]) => <SkillPair key={`language-${language1.name}-${language2.name}`} entry1={language1} entry2={language2} />)}
+          {breadth.map((catGroup) => {
+            const gamePairs = groupPairs(catGroup.games);
+            return (
+              <div key={`gamecat-${catGroup.name}`}>
+                <h3>{catGroup.name}</h3>
+                {gamePairs.map(([game1, game2]) => <SkillPair key={`game-${game1.name}-${game2.name}`} entry1={game1} entry2={game2} />)}
+              </div>
+            );
+          })}
         </Grid>
       </div>
       <div id="achievements">
@@ -55,8 +331,12 @@ function Skills() {
             quote="I master games."
           />
           <hr />
-          <h3>Languages</h3>
-          {languagePairs.map(([language1, language2]) => <SkillPair key={`language-${language1.name}-${language2.name}`} entry1={language1} entry2={language2} />)}
+          {mastery.map((catGroup) => (
+            <div key={`gamecat-${catGroup.name}`}>
+              <h3>{catGroup.name}</h3>
+              {catGroup.games.map((game1) => <SkillPair key={`game-${game1.name}`} entry1={game1} entry2={{}} />)}
+            </div>
+          ))}
         </Grid>
       </div>
     </div>
--- src/modules/submodules/SkillPair.jsx
+++ src/modules/submodules/SkillPair.jsx
@@ -17,37 +17,50 @@ const generateStars = (name, value) => {
 };
 
 function Skill({ entry1, entry2 }) {
-  return (
-    <Row>
-      <Col xs={8} md={4}>
+  const entry1Component = (
+    <div>
+      <Col xs={entry1.value ? 8 : 12} md={entry2.name ? (entry1.value ? 4 : 6) : 12}> {/* eslint-disable-line */}
         <h4>{entry1.name}</h4>
       </Col>
-      { typeof entry1.value === 'number'
-        ? (
-          <Col xs={4} md={2} className="value">
-            { generateStars(entry1.name, entry1.value) }
-          </Col>
-        )
-        : (
-          <Col xs={4} md={2} className="value">
-            { entry1.value }
-          </Col>
-        )}
+      { entry1.value
+        && (typeof entry1.value === 'number'
+          ? (
+            <Col xs={4} md={2} className="value">
+              { generateStars(entry1.name, entry1.value) }
+            </Col>
+          )
+          : (
+            <Col xs={4} md={2} className="value">
+              { entry1.value }
+            </Col>
+          )) }
+    </div>
+  );
 
-      <Col xs={8} md={4}>
+  const entry2Component = (
+    <div>
+      <Col xs={entry2.value ? 8 : 12} md={entry2.name ? (entry2.value ? 4 : 6) : 12}> {/* eslint-disable-line */}
         <h4>{entry2.name}</h4>
       </Col>
-      { typeof entry2.value === 'number'
-        ? (
-          <Col xs={4} md={2} className="value">
-            { generateStars(entry2.name, entry2.value) }
-          </Col>
-        )
-        : (
-          <Col xs={4} md={2} className="value">
-            { entry2.value }
-          </Col>
-        )}
+      { entry2.value
+        && (typeof entry2.value === 'number'
+          ? (
+            <Col xs={4} md={2} className="value">
+              { generateStars(entry2.name, entry2.value) }
+            </Col>
+          )
+          : (
+            <Col xs={4} md={2} className="value">
+              { entry2.value }
+            </Col>
+          )) }
+    </div>
+  );
+
+  return (
+    <Row>
+      { entry1Component }
+      { entry2.name ? entry2Component : null}
     </Row>
   );
 }
