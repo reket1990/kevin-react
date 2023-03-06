5768ac1e88e909903548358c24a49beeea034a06
Richard Truong <reket1990@yahoo.com>
2023-03-06 15:18:26 -0800

    cleanup

--- src/modules/submodules/Stats.jsx
+++ src/modules/submodules/Stats.jsx
@@ -46,14 +46,14 @@ class Stats extends React.Component {
 
   componentDidMount() {
     this.setState({ tabIndex: 0 }); // hack to force re-render to calculate width
-    this.postRender();
+    this.drawCanvas();
   }
 
   componentDidUpdate() {
-    this.postRender();
+    this.drawCanvas();
   }
 
-  postRender() {
+  drawCanvas() {
     const { tabIndex } = this.state;
     const setTabIndex = (index) => { this.setState({ tabIndex: index }); };
 
@@ -65,25 +65,12 @@ class Stats extends React.Component {
     const polygonSize = 120 * sizeMultiplier;
     const circleSize = 56 * sizeMultiplier;
 
-    let circles = [];
-
     // Get canvas context.
     const canvas = document.getElementById('statsCanvas');
     const ctx = canvas.getContext('2d');
 
-    function MouseHandler() {
-      const handler = this;
-      this.move = (e) => {
-        handler.x = e.clientX - canvas.getBoundingClientRect().left;
-        handler.y = e.clientY - canvas.getBoundingClientRect().top;
-      };
-      document.onmousemove = this.move;
-    }
-
     const vertices = [];
     function drawRegularPolygon(x, y, fill, stroke, strokeWidth, sides, radius) {
-      // Draws a regular polygon onto the canvas.
-
       // Variable declarations
       let arc;
       let point;
@@ -117,102 +104,107 @@ class Stats extends React.Component {
       // Return points array for future use
       return points;
     }
-    circles = [];
-    function redraw() {
-      circles = [];
 
-      const polygon = drawRegularPolygon(polygonX, polygonY, '#666', '#333', 2, statsData.length, polygonSize);
-      ctx.beginPath();
-      ctx.setLineDash([5]);
-      ctx.lineDashOffset = 10;
-      ctx.strokeStyle = '#333';
-      for (let i = 0; i < polygon.length; i += 1) {
-        ctx.moveTo(polygonX, polygonY);
-        ctx.lineTo(polygon[i].x, polygon[i].y);
-      }
-      ctx.stroke();
-      // Remove line dash for future use
-      ctx.setLineDash([0]);
-
-      // Inner polygon
-      ctx.beginPath();
-      let index;
-      let stat;
-      let text;
-      const innerPolygonVertices = [];
-      let x;
-      let y;
-      for (let i = 0; i < statsData.length + 1; i += 1) {
-        index = i % statsData.length;
-        if (vertices[index] === undefined)vertices[index] = {};
-        if (innerPolygonVertices[index] === undefined)innerPolygonVertices[index] = {};
-        vertices[index].x = polygon[index].x;
-        vertices[index].y = polygon[index].y;
-        stat = statsData[index].value;
-        const distX = vertices[index].x - polygonX;
-        const distY = vertices[index].y - polygonY;
-        vertices[index].distX = distX;
-        vertices[index].distY = distY;
-        vertices[index].distTotal = Math.sqrt(distX * distX + distY * distY);
-        vertices[index].radians = Math.atan2(distY, distX);
-        x = polygonX + Math.cos(vertices[index].radians) * (vertices[index].distTotal * stat / 100);
-        y = polygonY + Math.sin(vertices[index].radians) * (vertices[index].distTotal * stat / 100);
-        innerPolygonVertices[index].x = x;
-        innerPolygonVertices[index].y = y;
-        ctx.lineTo(x, y);
-      }
-      // Set alpha of polygon
-      ctx.globalAlpha = 0.5;
-      ctx.fillStyle = '#339933';
-      ctx.fill();
-
-      ctx.globalAlpha = 1;
-      ctx.strokeStyle = '#339933';
-      ctx.stroke();
-
-      // Draw circles;
-      for (let i = 0; i < statsData.length; i += 1) {
-        index = i;
-        x = vertices[index].x + Math.cos(vertices[index].radians) * (circleSize + 8);
-        y = vertices[index].y + Math.sin(vertices[index].radians) * (circleSize + 8);
-        ctx.beginPath();
-        ctx.arc(x, y, circleSize, 0, 2 * Math.PI, false);
-        ctx.fillStyle = '#333';
-        ctx.fill();
-        ctx.closePath();
-        ctx.beginPath();
-        ctx.arc(x, y, circleSize - 4, 0, 2 * Math.PI, false);
-        ctx.fillStyle = '#fff';
-        if (index === tabIndex)ctx.fillStyle = statsData[index].color;
-        ctx.fill();
-        ctx.closePath();
-        ctx.beginPath();
-        ctx.arc(x, y, circleSize - 6, 0, 2 * Math.PI, false);
-        ctx.fillStyle = statsData[index].color;
-        if (index === tabIndex)ctx.fillStyle = '#fff';
-        ctx.fill();
-        ctx.closePath();
-        circles.push({
-          x,
-          y,
-          size: circleSize - 6,
-          radius: (circleSize - 6) / 2,
-          stat: statsData[index].title,
-          color: statsData[index].color,
-        });
-        ctx.fillStyle = '#fff';
-        if (index === tabIndex)ctx.fillStyle = statsData[index].color;
-        ctx.font = '16px Open Sans';
-        text = statsData[index].title.toUpperCase();
-        stat = statsData[index].value;
-        ctx.fillText(text, x - ctx.measureText(text).width / 2, y);
-        ctx.fillText(stat, x - ctx.measureText(stat).width / 2, y + 16);
-      }
+    const polygon = drawRegularPolygon(polygonX, polygonY, '#666', '#333', 2, statsData.length, polygonSize);
+    ctx.beginPath();
+    ctx.setLineDash([5]);
+    ctx.lineDashOffset = 10;
+    ctx.strokeStyle = '#333';
+    for (let i = 0; i < polygon.length; i += 1) {
+      ctx.moveTo(polygonX, polygonY);
+      ctx.lineTo(polygon[i].x, polygon[i].y);
     }
-    redraw();
-    const mouse = new MouseHandler();
+    ctx.stroke();
+    // Remove line dash for future use
+    ctx.setLineDash([0]);
 
-    function loop() {
+    // Inner polygon
+    ctx.beginPath();
+    let index;
+    let stat;
+    let text;
+    const innerPolygonVertices = [];
+    let x;
+    let y;
+    for (let i = 0; i < statsData.length + 1; i += 1) {
+      index = i % statsData.length;
+      if (vertices[index] === undefined)vertices[index] = {};
+      if (innerPolygonVertices[index] === undefined)innerPolygonVertices[index] = {};
+      vertices[index].x = polygon[index].x;
+      vertices[index].y = polygon[index].y;
+      stat = statsData[index].value;
+      const distX = vertices[index].x - polygonX;
+      const distY = vertices[index].y - polygonY;
+      vertices[index].distX = distX;
+      vertices[index].distY = distY;
+      vertices[index].distTotal = Math.sqrt(distX * distX + distY * distY);
+      vertices[index].radians = Math.atan2(distY, distX);
+      x = polygonX + Math.cos(vertices[index].radians) * (vertices[index].distTotal * stat / 100);
+      y = polygonY + Math.sin(vertices[index].radians) * (vertices[index].distTotal * stat / 100);
+      innerPolygonVertices[index].x = x;
+      innerPolygonVertices[index].y = y;
+      ctx.lineTo(x, y);
+    }
+
+    // Set alpha of polygon
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = '#339933';
+    ctx.fill();
+    ctx.globalAlpha = 1;
+    ctx.strokeStyle = '#339933';
+    ctx.stroke();
+
+    // Draw circles;
+    const circles = [];
+    for (let i = 0; i < statsData.length; i += 1) {
+      index = i;
+      x = vertices[index].x + Math.cos(vertices[index].radians) * (circleSize + 8);
+      y = vertices[index].y + Math.sin(vertices[index].radians) * (circleSize + 8);
+      ctx.beginPath();
+      ctx.arc(x, y, circleSize, 0, 2 * Math.PI, false);
+      ctx.fillStyle = '#333';
+      ctx.fill();
+      ctx.closePath();
+      ctx.beginPath();
+      ctx.arc(x, y, circleSize - 4, 0, 2 * Math.PI, false);
+      ctx.fillStyle = '#fff';
+      if (index === tabIndex)ctx.fillStyle = statsData[index].color;
+      ctx.fill();
+      ctx.closePath();
+      ctx.beginPath();
+      ctx.arc(x, y, circleSize - 6, 0, 2 * Math.PI, false);
+      ctx.fillStyle = statsData[index].color;
+      if (index === tabIndex)ctx.fillStyle = '#fff';
+      ctx.fill();
+      ctx.closePath();
+      circles.push({
+        x,
+        y,
+        size: circleSize - 6,
+        radius: (circleSize - 6) / 2,
+        stat: statsData[index].title,
+        color: statsData[index].color,
+      });
+      ctx.fillStyle = '#fff';
+      if (index === tabIndex)ctx.fillStyle = statsData[index].color;
+      ctx.font = '16px Open Sans';
+      text = statsData[index].title.toUpperCase();
+      stat = statsData[index].value;
+      ctx.fillText(text, x - ctx.measureText(text).width / 2, y);
+      ctx.fillText(stat, x - ctx.measureText(stat).width / 2, y + 16);
+    }
+
+    // Circle hover detection
+    function MouseHandler() {
+      const handler = this;
+      this.move = (e) => {
+        handler.x = e.clientX - canvas.getBoundingClientRect().left;
+        handler.y = e.clientY - canvas.getBoundingClientRect().top;
+      };
+      document.onmousemove = this.move;
+    }
+    const mouse = new MouseHandler();
+    function detectHoverLoop() {
       let shouldLoop = true;
       for (let i = 0; i < circles.length; i += 1) {
         const circle = circles[i];
@@ -220,16 +212,15 @@ class Stats extends React.Component {
         const distY = circle.y - mouse.y;
         const distTotal = Math.sqrt(distX * distX + distY * distY);
         if (distTotal < circle.size) {
-          redraw();
           setTabIndex(i);
           shouldLoop = false;
         }
       }
       if (shouldLoop) {
-        setTimeout(loop, 100);
+        setTimeout(detectHoverLoop, 100);
       }
     }
-    loop();
+    detectHoverLoop();
   }
 
   render() {
